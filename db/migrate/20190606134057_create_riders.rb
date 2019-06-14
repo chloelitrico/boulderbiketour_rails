84414c1cb5b905d531c55873ee3113f63baa7688
Chloe Litrico <chloelitrico@Chloes-MacBook-Pro.local>
2019-06-14 16:48:15 +0200

    First commit

---
+++ db/migrate/20190606134057_create_riders.rb
@@ -0,0 +1,14 @@
+class CreateRiders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :riders do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :city
+      t.string :state
+      t.decimal :latitude
+      t.decimal :longitude
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+Rider.destroy_all
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'rider_data.csv'))
+csv = CSV.parse(csv_text, :headers => false, :encoding => 'ISO-8859-1')
+csv.each_with_index  do |row, i|
+    next if i == 0
+    t = Rider.new
+    t.first_name = row[0]
+    t.last_name = row[1]
+    t.city = row[2]
+    t.state = row[3]
+    t.latitude = row[4]
+    t.longitude = row[5]
+    t.save
+    puts "#{t.first_name} saved"
+end
+
+puts "There are now #{Rider.count} rows in the table"
